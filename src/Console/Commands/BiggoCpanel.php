72dd6b3f6b1046659524d6522b23bff317dbc293
Joram Kimata <joram.kimata@ega.go.tz>
2017-08-31 13:18:16 +0300

    edited command file

--- src/Console/Commands/BiggoCpanel.php
+++ src/Console/Commands/BiggoCpanel.php
@@ -54,6 +54,7 @@ class BiggoCpanel extends Command
 			$this->info('Let Us Go Together!');
 			$this->info('');
 			$this->info('...');
+
 			$files = glob(base_path() . '/app');
 			Zipper::make(base_path() . '/build/site/app.zip')->add($files)->close();
 			$path2 = base_path() . '/build/site/app';		
@@ -62,7 +63,62 @@ class BiggoCpanel extends Command
 				$result = File::makeDirectory($path2);	
 			}	
 			Zipper::make(base_path() . '/build/site/app.zip')->extractTo(base_path() . '/build/site/app');
+
+			$filesX_ = glob(base_path() . '/config');
+			Zipper::make(base_path() . '/build/site/config.zip')->add($filesX_)->close();
+			$path2X_ = base_path() . '/build/site/config';		
+			if(!File::exists($path2X_)){
+				$this->info('... ...');
+				$result = File::makeDirectory($path2X_);	
+			}	
+			Zipper::make(base_path() . '/build/site/config.zip')->extractTo(base_path() . '/build/site/config');
+
+			$filesX_2 = glob(base_path() . '/database');
+			Zipper::make(base_path() . '/build/site/database.zip')->add($filesX_2)->close();
+			$path2X_2 = base_path() . '/build/site/database';		
+			if(!File::exists($path2X_2)){
+				$this->info('... ...');
+				$result = File::makeDirectory($path2X_2);	
+			}	
+			Zipper::make(base_path() . '/build/site/database.zip')->extractTo(base_path() . '/build/site/database');
 			
+            $filesX_21 = glob(base_path() . '/resources');
+			Zipper::make(base_path() . '/build/site/resources.zip')->add($filesX_21)->close();
+			$path2X_21 = base_path() . '/build/site/resources';		
+			if(!File::exists($path2X_21)){
+				$this->info('... ...');
+				$result = File::makeDirectory($path2X_21);	
+			}	
+			Zipper::make(base_path() . '/build/site/resources.zip')->extractTo(base_path() . '/build/site/resources');
+
+            $filesX_21_ = glob(base_path() . '/routes');
+			Zipper::make(base_path() . '/build/site/routes.zip')->add($filesX_21_)->close();
+			$path2X_21_ = base_path() . '/build/site/routes';		
+			if(!File::exists($path2X_21_)){
+				$this->info('... ...');
+				$result = File::makeDirectory($path2X_21_);	
+			}	
+			Zipper::make(base_path() . '/build/site/routes.zip')->extractTo(base_path() . '/build/site/routes');
+
+            $filesX_21_1 = glob(base_path() . '/storage');
+			Zipper::make(base_path() . '/build/site/storage.zip')->add($filesX_21_1)->close();
+			$path2X_21_1 = base_path() . '/build/site/storage';		
+			if(!File::exists($path2X_21_1)){
+				$this->info('... ...');
+				$result = File::makeDirectory($path2X_21_1);	
+			}	
+			Zipper::make(base_path() . '/build/site/storage.zip')->extractTo(base_path() . '/build/site/storage');
+
+            $filesX_21_12 = glob(base_path() . '/tests');
+			Zipper::make(base_path() . '/build/site/tests.zip')->add($filesX_21_12)->close();
+			$path2X_21_12 = base_path() . '/build/site/tests';		
+			if(!File::exists($path2X_21_12)){
+				$this->info('... ...');
+				$result = File::makeDirectory($path2X_21_12);	
+			}	
+			Zipper::make(base_path() . '/build/site/tests.zip')->extractTo(base_path() . '/build/site/tests');
+
+
 			$this->info('... ... ...');
 			$files_ = glob(base_path() . '/bootstrap');
 			Zipper::make(base_path() . '/build/site/bootstrap.zip')->add($files_)->close();
@@ -127,7 +183,7 @@ class BiggoCpanel extends Command
 				$content_2 = @file_get_contents( base_path() . "/database.php.build");
 				
 				file_put_contents(base_path() . "/build/index.php" , $content_1);
-				file_put_contents(base_path() . "/build/site/app/config/database.php" , $content_2);
+				file_put_contents(base_path() . "/build/site/config/database.php" , $content_2);
 				
 				$sourceDir = base_path() . "/vendor";
 				$destinationDir = base_path() . '/build/site/vendor';
